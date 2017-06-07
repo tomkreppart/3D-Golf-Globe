fccbfd02c6e835bc6867bdafd414592820bdfbe3
Tom Krepp <tommykrepp@aol.com>
2017-06-07 17:23:42 -0600

    data

--- public/js/globe/ngGlobe.js
+++ public/js/globe/ngGlobe.js
@@ -279,3 +279,50 @@
     angular.module('app').directive('ngGlobe', ngGlobe);
 
 }());
+
+
+// function calcPosFromLatLonRad(lat,lon,radius){
+//
+// var phi   = (90-lat)*(Math.PI/180);
+// var theta = (lon+180)*(Math.PI/180);
+//
+// x = -((radius) * Math.sin(phi)*Math.cos(theta));
+// z = ((radius) * Math.sin(phi)*Math.sin(theta));
+// y = ((radius) * Math.cos(phi));
+//
+//
+// 	console.log([x,y,z]);
+//    return [x,y,z];
+// }
+
+
+// var createDataPoints = function(){
+//
+// meshes=[];
+// for(var i = 0 ; i < 10 ; i++){
+//
+// 	var geometry	= new THREE.SphereGeometry(0.025, 20, 20)
+// 	var material	= new THREE.MeshBasicMaterial({
+// 		color: new THREE.Color('white')
+// 	})
+// 	var mesh	= new THREE.Mesh(geometry, material)
+// 	meshes.push(mesh);
+// }
+
+
+// latlons = [[40.7142700,-74.0059700], [52.5243700,13.4105300]];
+// function addPoints(){
+//  var meshes = createDataPoints();
+//  for(var i = 0; i< meshes.length; i++ ){
+//    mesh = meshes[i];
+//  currentMesh.add(mesh)
+//
+//  latlon=latlons[Math.floor(Math.random()*latlons.length)];
+//
+//  latlonpoint = calcPosFromLatLonRad(latlon[0],latlon[1], 0.5);
+//  mesh.position = new THREE.Vector3(latlonpoint[0],latlonpoint[1],latlonpoint[2]);
+//  }
+//
+// }
+//
+// addPoints();
